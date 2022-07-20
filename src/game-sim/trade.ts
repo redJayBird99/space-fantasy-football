58b4ceef4ac2e51f6f1c2ed59a8aeae02f05d602
D. Davide <academyckc@gmail.com>
2022-07-20 22:06:30 +0200

    add trade

---
+++ src/game-sim/trade.ts
@@ -0,0 +1,209 @@
+import { Player, SALARY_CAP } from "../character/player";
+import { GsTm, sumWages, Team } from "../character/team";
+import { shuffle } from "../util/generator";
+import { dist } from "../util/math";
+
+type TradeSide = { by: Team; content: Player[] };
+type Trade = { side1: TradeSide; side2: TradeSide };
+
+const MAX_EXCHANGE_SIZE = 3; // only for the non user teams, it makes the findOffer faster and the exchange smaller
+const MIN_TEAM_SIZE = 18;
+const MAX_TEAM_SIZE = 30;
+
+/**
+ * @returns true if after the exchange the team players size is under the requirement
+ */
+function underMinTeamSize(t: Team, get: Player[], give: Player[]): boolean {
+  return t.playerIds.length + get.length - give.length < MIN_TEAM_SIZE;
+}
+
+/**
+ * @returns true if after the exchange the team players size is over the requirement
+ */
+function overMaxTeamSize(t: Team, get: Player[], give: Player[]): boolean {
+  return t.playerIds.length + get.length - give.length > MAX_TEAM_SIZE;
+}
+
+/**
+ * @returns true when the exchange respects the team players size requirements
+ * or get closer to the size requirements
+ */
+function validTeamSize(t: Team, get: Player[], give: Player[]): boolean {
+  if (underMinTeamSize(t, get, give)) {
+    return get.length >= give.length;
+  } else if (overMaxTeamSize(t, get, give)) {
+    return give.length >= get.length;
+  }
+
+  return true;
+}
+
+/**
+ * @returns true when a and b are within a 10% distance of each other
+ */
+function areClose(a: number, b: number): boolean {
+  const d = dist(a, b);
+  return d <= Math.abs(a) * 0.1 && d <= Math.abs(b) * 0.1;
+}
+
+/**
+ * @param mean of the players population
+ * @param stdDev standardDeviation of the players population
+ * @param score of a player
+ * @returns a rating of the given score
+ */
+function scoreAppeal(mean: number, stdDev: number, score: number): number {
+  const step = stdDev / 4;
+  return 2 ** ((score - mean) / step);
+}
+
+/**
+ * get a shew mean towards the larger value in the array
+ */
+function skewMean(a: number[]): number {
+  return Math.max(...a) * 0.5 + (a.reduce((a, b) => a + b, 0) / a.length) * 0.5;
+}
+
+/**
+ * @param plsScores all the offered player scores
+ * @returns a rating of the offer or NaN when plsScores is empty
+ */
+function offerAppeal({ gs, t }: GsTm, plsScores: number[]): number {
+  if (plsScores.length === 0) {
+    return NaN;
+  }
+
+  const { meanScore: mean, standardDev: stdDev } = gs.popStats;
+  // reminder: the step size in scoreAppeal is very influential on the balance
+  // betweeen the mean and player scores, with a smaller step the mean get more influential
+  return (
+    scoreAppeal(mean, stdDev, skewMean(plsScores)) +
+    plsScores.reduce((a, v) => a + scoreAppeal(mean, stdDev, v), 0)
+  );
+}
+
+/**
+ * returns a function that checks if the team payroll stay under the salary cap
+ * or it is reduced when exchanging the getting and leaving players
+ */
+function affordable(g: GsTm): (get: Player[], give: Player[]) => boolean {
+  const payroll = Team.getWagesAmount(g);
+
+  return (get, give) => {
+    const newPayroll = payroll + sumWages(g.gs, get) - sumWages(g.gs, give);
+    return newPayroll < SALARY_CAP || newPayroll <= payroll;
+  };
+}
+
+/**
+ * returns true when the team judges the exchange acceptable and it respects
+ * salary cap and team size requirements
+ * @param get the players entering the team
+ * @param give the players leaving the team
+ */
+function acceptable({ gs, t }: GsTm, get: Player[], give: Player[]): boolean {
+  if (!get.length || !give.length || !validTeamSize(t, get, give)) {
+    return false;
+  }
+
+  const getScores = get.map((p) => Team.evaluatePlayer({ gs, t, p }));
+  const giveScores = give.map((p) => Team.evaluatePlayer({ gs, t, p }));
+  const getAppeal = offerAppeal({ gs, t }, getScores);
+  const giveAppeal = offerAppeal({ gs, t }, giveScores);
+
+  return (
+    affordable({ gs, t })(get, give) &&
+    (getAppeal > giveAppeal || areClose(getAppeal, giveAppeal))
+  );
+}
+
+/**
+ * returns a closely matching offer to the getting one by the team
+ * the returned offer respects salary cap and team size requirements for the
+ * given team (no check for the other team)
+ * the function is indeterministic
+ * @param get what the team get in exchange
+ * @returns a empty array when no offer was found
+ */
+function findOffer({ gs, t }: GsTm, get: Player[]): Player[] {
+  // TOFIX: this is the subset sum problem we uses a brute force solution (find combinations)
+  // shuffle to make it indeterministic and slice for performance
+  const plrs = shuffle(Team.getNotExipiringPlayers({ gs, t })).slice(0, 20);
+  const _affordable = affordable({ gs, t });
+  const plrsAppeal = new Map<Player, number>();
+  plrs.forEach((p) => plrsAppeal.set(p, Team.evaluatePlayer({ gs, t, p })));
+  const getAppeal = offerAppeal(
+    { gs, t },
+    get.map((p) => Team.evaluatePlayer({ gs, t, p }))
+  );
+  const rst: Player[][] = [];
+
+  // MAX_EXCHANGE_SIZE solution are more frequent so to balance it we search all
+  // solution (instead of the first one) and pick only one for each size
+  const search = (stack: Player[], i: number): Player[] | void => {
+    if (MAX_EXCHANGE_SIZE < stack.length || !validTeamSize(t, get, stack)) {
+      return;
+    }
+
+    if (stack.length > 0 && !rst[stack.length - 1]) {
+      const giveAppeal = offerAppeal(
+        { gs, t },
+        stack.map((p) => plrsAppeal.get(p)!)
+      );
+
+      if (areClose(giveAppeal, getAppeal) && _affordable(get, stack)) {
+        rst[stack.length - 1] = stack.slice();
+      }
+    }
+
+    for (let j = i; j < plrs.length; j++) {
+      stack.push(plrs[i]);
+      search(stack, j + 1);
+      stack.pop();
+    }
+  };
+
+  search([], 0);
+  return shuffle(rst.filter((a) => a))[0] ?? [];
+}
+
+/**
+ * search for an acceptable trade between the two given teams, the output
+ * is random multiple calls gives different results,
+ * because team can't often agree on the players value a trade is somehow rare
+ * @param other the team trading with
+ */
+function seachTrade({ gs, t }: GsTm, other: Team): Trade | void {
+  // TOFIX it is a temp solution
+  for (let attempt = 0; attempt < 3; attempt++) {
+    const get = shuffle(Team.getNotExipiringPlayers({ gs, t: other })).slice(
+      0,
+      Math.floor(MAX_EXCHANGE_SIZE * Math.random()) + 1
+    );
+    const give = findOffer({ gs, t }, get);
+
+    if (acceptable({ gs, t: other }, give, get)) {
+      return {
+        side1: { by: t, content: give },
+        side2: { by: other, content: get },
+      };
+    }
+  }
+}
+
+export {
+  MAX_TEAM_SIZE,
+  MIN_TEAM_SIZE,
+  MAX_EXCHANGE_SIZE,
+  underMinTeamSize,
+  overMaxTeamSize,
+  validTeamSize,
+  areClose,
+  scoreAppeal,
+  skewMean,
+  offerAppeal,
+  affordable,
+  acceptable,
+  findOffer,
+  seachTrade,
+};
---
+++ test/game-sim/trade.test.ts
@@ -0,0 +1,300 @@
+import * as _t from "../../src/character/team";
+import * as _p from "../../src/character/player";
+import * as _gs from "../../src/game-state/game-state";
+import * as _trde from "../../src/game-sim/trade";
+import * as _u from "../../src/character/util";
+import { mean } from "../../src/util/generator";
+
+// guarantee findoffer
+const getSampleOffer = (gs: _gs.GameState, by: _t.Team, to: _t.Team) => {
+  const pls = _t.Team.getNotExipiringPlayers({ gs, t: to });
+  let get = [pls[0], pls[1]];
+  let offer = _trde.findOffer({ gs, t: by }, get);
+
+  for (let i = 0; i < pls.length && offer.length === 0; i++) {
+    get = [pls[i]];
+    offer = _trde.findOffer({ gs, t: by }, get);
+  }
+
+  return { by, offer, get };
+};
+
+describe("underMinTeamSize", () => {
+  const st = _gs.GameState.init(["someName", "other"]);
+  const team = st.teams.someName;
+  const team2 = st.teams.other;
+  const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+  const tm2Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 });
+
+  test("when stay over the min team size return false", () => {
+    expect(_trde.underMinTeamSize(team, [tm2Pls[0]], [tm1Pls[0]])).toBe(false);
+  });
+
+  test("when get under the min team size return true", () => {
+    const giveAmount = tm1Pls.length - _trde.MIN_TEAM_SIZE + 2;
+    const give = tm1Pls.slice(0, giveAmount);
+    expect(_trde.underMinTeamSize(team, [tm2Pls[0]], give)).toBe(true);
+  });
+});
+
+describe("overMaxTeamSize", () => {
+  const st = _gs.GameState.init(["someName", "other"]);
+  const team = st.teams.someName;
+  const team2 = st.teams.other;
+  const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+  const tm2Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 });
+
+  test("when stay under the max team size return false", () => {
+    expect(_trde.overMaxTeamSize(team, [tm2Pls[0]], [tm1Pls[0]])).toBe(false);
+  });
+
+  test("when get over the max team size return true", () => {
+    const getAmount = _trde.MAX_TEAM_SIZE - tm2Pls.length + 2;
+    const get = tm1Pls.slice(0, getAmount);
+    expect(_trde.overMaxTeamSize(team, get, [tm1Pls[0]])).toBe(true);
+  });
+});
+
+describe("validTeamSize", () => {
+  const st = _gs.GameState.init(["someName", "other"]);
+  const team = st.teams.someName;
+  const team2 = st.teams.other;
+
+  test("when is over the team max size but the team size is reduced return true", () => {
+    const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+    const pls = _u.rdmPlayers(_trde.MAX_TEAM_SIZE - tm1Pls.length + 3);
+    pls.forEach((p) => {
+      _gs.GameState.savePlayer(st, p);
+      _t.Team.signPlayer({ gs: st, t: team, p }, _p.MIN_WAGE);
+    });
+    const gv = _t.Team.getNotExipiringPlayers({ gs: st, t: team }).slice(0, 2);
+    const gt = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 }).slice(0, 1);
+    expect(_trde.validTeamSize(team, gt, gv)).toBe(true);
+  });
+
+  test("when is over the max team size return false", () => {
+    const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+    const pls = _u.rdmPlayers(_trde.MAX_TEAM_SIZE - tm1Pls.length + 3);
+    pls.forEach((p) => {
+      _gs.GameState.savePlayer(st, p);
+      _t.Team.signPlayer({ gs: st, t: team, p }, _p.MIN_WAGE);
+    });
+    const gv = _t.Team.getNotExipiringPlayers({ gs: st, t: team }).slice(0, 1);
+    const gt = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 }).slice(0, 2);
+    expect(_trde.validTeamSize(team, gt, gv)).toBe(false);
+  });
+
+  test("when is under the team min size but the team size is increased return true", () => {
+    const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+    tm1Pls
+      .slice(0, tm1Pls.length + 2 - _trde.MIN_TEAM_SIZE)
+      .forEach((p) =>
+        _t.Team.unsignPlayer(st, _gs.GameState.getContract(st, p)!)
+      );
+    const gv = _t.Team.getNotExipiringPlayers({ gs: st, t: team }).slice(0, 1);
+    const gt = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 }).slice(0, 2);
+    expect(_trde.validTeamSize(team, gt, gv)).toBe(true);
+  });
+
+  test("when is under the team min size return false", () => {
+    const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+    tm1Pls
+      .slice(0, tm1Pls.length + 2 - _trde.MIN_TEAM_SIZE)
+      .forEach((p) =>
+        _t.Team.unsignPlayer(st, _gs.GameState.getContract(st, p)!)
+      );
+    const gv = _t.Team.getNotExipiringPlayers({ gs: st, t: team }).slice(0, 2);
+    const gt = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 }).slice(0, 1);
+    expect(_trde.validTeamSize(team, gt, gv)).toBe(false);
+  });
+});
+
+describe("areClose", () => {
+  test("return true when a and b are within a 10% distance between each other", () => {
+    expect(_trde.areClose(10, 9.5)).toBe(true);
+  });
+
+  test("return false when a and b are too far apart", () => {
+    expect(_trde.areClose(9, 10)).toBe(false);
+  });
+});
+
+describe("scoreAppeal", () => {
+  test("when mean: 65, stdDev: 5 and score: 67 should return ", () => {
+    expect(_trde.scoreAppeal(65, 5, 67)).toBeCloseTo(3.031);
+  });
+
+  test("when mean: 65, stdDev: 6 and score: 60 should return ", () => {
+    expect(_trde.scoreAppeal(65, 6, 60)).toBeCloseTo(0.099);
+  });
+});
+
+describe("skewMean", () => {
+  test("should return a value equal to the mean when the values in the sample are all equal", () => {
+    const sample = [3, 3, 3, 3, 3];
+    expect(mean(sample)).toBeCloseTo(_trde.skewMean(sample));
+  });
+
+  test("should return a value greater than the mean", () => {
+    const sample = [1, 2, 4, 8, 16];
+    expect(mean(sample)).toBeLessThan(_trde.skewMean(sample));
+  });
+});
+
+describe("affordable()", () => {
+  const st = _gs.GameState.init(["someName", "other"]);
+  const team = st.teams.someName;
+  const team2 = st.teams.other;
+  const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+  const tm2Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 });
+
+  describe("team payroll is over salary cap", () => {
+    test("when get wages are greater than out wages should return false", () => {
+      _u.getContracts(st, team).forEach((c) => (c.wage = _p.MAX_WAGE));
+      _u.getContracts(st, team2).forEach((c) => (c.wage = 2 * _p.MAX_WAGE));
+      const out = tm1Pls.slice(0, 5);
+      const get = tm2Pls.slice(0, 5);
+      expect(_trde.affordable({ gs: st, t: team })(get, out)).toBe(false);
+    });
+
+    test("when get wages are less than out wages should return true", () => {
+      _u.getContracts(st, team).forEach((c) => (c.wage = _p.MAX_WAGE));
+      _u.getContracts(st, team2).forEach((c) => (c.wage = _p.MAX_WAGE / 2));
+      const out = tm1Pls.slice(0, 5);
+      const get = tm2Pls.slice(0, 5);
+      expect(_trde.affordable({ gs: st, t: team })(get, out)).toBe(true);
+    });
+  });
+
+  describe("team payroll is under salary cap", () => {
+    test("when the payroll stay under the salary cap after adding get wages should return true", () => {
+      _u.getContracts(st, team).forEach((c) => (c.wage = _p.MIN_WAGE));
+      _u.getContracts(st, team2).forEach((c) => (c.wage = 2 * _p.MIN_WAGE));
+      const out = tm1Pls.slice(0, 5);
+      const get = tm2Pls.slice(0, 5);
+      expect(_trde.affordable({ gs: st, t: team })(get, out)).toBe(true);
+    });
+
+    test("when the payroll go over the salary cap after adding get wages should return false", () => {
+      _u.getContracts(st, team).forEach((c) => (c.wage = _p.MIN_WAGE));
+      _u.getContracts(st, team2).forEach((c) => (c.wage = _p.SALARY_CAP));
+      const out = tm1Pls.slice(0, 5);
+      const get = tm2Pls.slice(0, 5);
+      expect(_trde.affordable({ gs: st, t: team })(get, out)).toBe(false);
+    });
+  });
+});
+
+describe("acceptable()", () => {
+  const st = _gs.GameState.init(["someName", "other"]);
+  const team = st.teams.someName;
+  const team2 = st.teams.other;
+  const tm1Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team });
+  const tm2Pls = _t.Team.getNotExipiringPlayers({ gs: st, t: team2 });
+
+  test("when get offer is an empty array should return false", () => {
+    const give = tm1Pls.slice(0, 5);
+    expect(_trde.acceptable({ gs: st, t: team }, [], give)).toBe(false);
+  });
+
+  test("when give offer is an empty array should return false", () => {
+    const get = tm2Pls.slice(0, 5);
+    expect(_trde.acceptable({ gs: st, t: team }, get, [])).toBe(false);
+  });
+
+  test("should return false when the team get under the min team size", () => {
+    const giveAmount = tm1Pls.length - _trde.MIN_TEAM_SIZE + 2;
+    const give = tm1Pls.slice(0, giveAmount);
+    const get = tm2Pls.slice(0, 1);
+    expect(_trde.acceptable({ gs: st, t: team }, get, give)).toBe(false);
+  });
+
+  test("should return false when the team get over the max team size", () => {
+    const getAmount = _trde.MAX_TEAM_SIZE + 2 - tm1Pls.length;
+    const get = tm2Pls.slice(0, getAmount);
+    const give = tm1Pls.slice(0, 1);
+    expect(_trde.acceptable({ gs: st, t: team }, get, give)).toBe(false);
+  });
+
+  test("when a fair affordable offer is made should return true", () => {
+    const { by, offer, get } = getSampleOffer(st, team2, team);
+    expect(_trde.acceptable({ gs: st, t: by }, get, offer)).toBe(true);
+  });
+
+  test("when a unfair offer is made should return false", () => {
+    const give = [tm1Pls[0]];
+    const get = [tm2Pls[0]];
+    _u.setSkillsTo(give[0], _p.MAX_SKILL);
+    _u.setSkillsTo(get[0], _p.MIN_SKILL);
+    expect(_trde.acceptable({ gs: st, t: team }, get, give)).toBe(false);
+  });
+
+  test("when a offer put the team over the salary cap should return false", () => {
+    const give = [tm1Pls[0]];
+    const get = [tm2Pls[0]];
+    _u.setSkillsTo(give[0], _p.MAX_SKILL);
+    _u.setSkillsTo(get[0], _p.MIN_SKILL);
+    _gs.GameState.getContract(st, get[0])!.wage = _p.SALARY_CAP;
+    expect(_trde.acceptable({ gs: st, t: team }, get, give)).toBe(false);
+  });
+});
+
+describe("findOffer()", () => {
+  const gs = _gs.GameState.init(["someName", "other"]);
+  const team = gs.teams.someName;
+  const team2 = gs.teams.other;
+  // for most of the tests we want a successfull findOffer call
+  const { by, offer, get } = getSampleOffer(gs, team2, team);
+
+  test("the found offer should be not empty", () => {
+    expect(_trde.affordable({ gs, t: by })(get, offer)).not.toEqual([]);
+  });
+
+  test("the found offer should should be considered acceptable by the offerer", () => {
+    expect(_trde.acceptable({ gs, t: by }, get, offer)).toBe(true);
+  });
+
+  test("the found offer should respect team size requirements", () => {
+    expect(_trde.underMinTeamSize(by, get, offer)).toBe(false);
+  });
+
+  test("the found offer should respect salary cap requirements", () => {
+    expect(_trde.affordable({ gs, t: by })(get, offer)).toBe(true);
+  });
+
+  test("when no offer was found return a empty array", () => {
+    // assuming that the get offer is unmatchable
+    const get = _t.Team.getNotExipiringPlayers({ gs, t: team }).slice(
+      0,
+      _trde.MAX_EXCHANGE_SIZE
+    );
+    get.forEach((p) => _u.setSkillsTo(p, _p.MAX_SKILL));
+    expect(_trde.findOffer({ gs, t: team2 }, get)).toEqual([]);
+  });
+});
+
+describe("searchTrade", () => {
+  describe("when is able to find a trade", () => {
+    let gs = _gs.GameState.init(["a", "b"]);
+    let trade = _trde.seachTrade({ gs, t: gs.teams.a }, gs.teams.b);
+
+    while (!trade) {
+      gs = _gs.GameState.init(["a", "b"]);
+      // make them more probable to agree
+      gs.teams.a.scoutOffset = 0;
+      gs.teams.b.scoutOffset = 0;
+      trade = _trde.seachTrade({ gs, t: gs.teams.a }, gs.teams.b);
+    }
+
+    const { side1, side2 } = trade;
+
+    test("the trade should be acceptable by both teams", () => {
+      expect(
+        _trde.acceptable({ gs, t: side1.by }, side2.content, side1.content)
+      ).toBe(true);
+      expect(
+        _trde.acceptable({ gs, t: side2.by }, side1.content, side2.content)
+      ).toBe(true);
+    });
+  });
+});
